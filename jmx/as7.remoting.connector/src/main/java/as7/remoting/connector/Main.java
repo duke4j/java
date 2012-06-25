cbb051f4038b412efd1d49595fd88ce9a78527ba
Yoshikazu YAMADA <tigre.lilium@gmail.com>
2012-06-26 02:11:58 +0900

    initial release

---
+++ jmx/as7.remoting.connector/src/main/java/as7/remoting/connector/Main.java
@@ -0,0 +1,13 @@
+package as7.remoting.connector;
+
+/**
+ * @author <a href="mailto:tigre.lilium@gmail.com">Yoshikazu YAMADA</a>
+ * @since Jun 26, 2012
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("You made it!");
+    }
+
+}
