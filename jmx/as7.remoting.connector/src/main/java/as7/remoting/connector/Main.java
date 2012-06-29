3ed34d634876c479414f8ad1f10aad409965e573
duke4j <tigre.lilium@gmail.com>
2012-06-29 20:20:54 +0900

    test release

--- jmx/as7.remoting.connector/src/main/java/as7/remoting/connector/Main.java
+++ jmx/as7.remoting.connector/src/main/java/as7/remoting/connector/Main.java
@@ -1,13 +1,37 @@
 package as7.remoting.connector;
 
+import javax.management.MBeanServerConnection;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
 /**
  * @author <a href="mailto:tigre.lilium@gmail.com">Yoshikazu YAMADA</a>
  * @since Jun 26, 2012
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("You made it!");
+    public static void main(String[] args) throws Exception {
+
+        String host = "localhost";
+        String port = "9999";
+
+        String urlString = System.getProperty("jmx.service.url",
+                "service:jmx:remoting-jmx://" + host + ":" + port);
+
+        JMXServiceURL serviceURL = new JMXServiceURL(urlString);
+        JMXConnector jmxConnector = JMXConnectorFactory.connect(serviceURL,
+                null);
+        MBeanServerConnection connection = jmxConnector
+                .getMBeanServerConnection();
+        try {
+
+            //Invoke on the JBoss AS MBean server
+            int count = connection.getMBeanCount();
+            System.out.println(count);
+        } finally {
+            jmxConnector.close();
+        }
     }
 
 }
