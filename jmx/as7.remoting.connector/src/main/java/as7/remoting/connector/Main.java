d9ab1bac5e66ac57badc163c2961cde9b16c7ca8
duke4j <tigre.lilium@gmail.com>
2012-07-02 19:31:40 +0900

    1.0 released

--- jmx/as7.remoting.connector/src/main/java/as7/remoting/connector/Main.java
+++ jmx/as7.remoting.connector/src/main/java/as7/remoting/connector/Main.java
@@ -1,5 +1,9 @@
 package as7.remoting.connector;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.management.MBeanServerConnection;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
@@ -11,27 +15,32 @@ import javax.management.remote.JMXServiceURL;
  */
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
 
-        String host = "localhost";
-        String port = "9999";
+        String urlString = "service:jmx:remoting-jmx://"
+                + System.getProperty("jmx.service.url", "localhost:9999");
 
-        String urlString = System.getProperty("jmx.service.url",
-                "service:jmx:remoting-jmx://" + host + ":" + port);
+        String[] credentials = {
+                System.getProperty("jboss.admin.name", "admin"),
+                System.getProperty("jboss.admin.password", "jboss") };
+
+        Map<String, Object> env = new HashMap<String, Object>();
+        env.put(JMXConnectorFactory.PROTOCOL_PROVIDER_PACKAGES,
+                "org.jboss.remotingjmx.RemotingConnectorProvider");
+        env.put(JMXConnector.CREDENTIALS, credentials);
 
         JMXServiceURL serviceURL = new JMXServiceURL(urlString);
-        JMXConnector jmxConnector = JMXConnectorFactory.connect(serviceURL,
-                null);
-        MBeanServerConnection connection = jmxConnector
-                .getMBeanServerConnection();
+        JMXConnector jmxConnector = JMXConnectorFactory
+                .connect(serviceURL, env);
         try {
-
-            //Invoke on the JBoss AS MBean server
+            MBeanServerConnection connection = jmxConnector
+                    .getMBeanServerConnection();
             int count = connection.getMBeanCount();
-            System.out.println(count);
+            System.out.println("A long time ago in a galaxy far, far away.... "
+                    + count + " MBeans in the galaxy....");
         } finally {
             jmxConnector.close();
         }
-    }
 
+    }
 }
