d0a197887729fb17b7f4e8a6e449f0c1dbcfc709
duke4j <tigre.lilium@gmail.com>
2012-07-03 10:40:34 +0900

    retrieving ObjectName

--- jmx/as7.remoting.connector/src/main/java/as7/remoting/connector/Main.java
+++ jmx/as7.remoting.connector/src/main/java/as7/remoting/connector/Main.java
@@ -1,10 +1,11 @@
 package as7.remoting.connector;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.management.MBeanInfo;
 import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
@@ -15,7 +16,7 @@ import javax.management.remote.JMXServiceURL;
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         String urlString = "service:jmx:remoting-jmx://"
                 + System.getProperty("jmx.service.url", "localhost:9999");
@@ -23,6 +24,14 @@ public class Main {
         String[] credentials = {
                 System.getProperty("jboss.admin.name", "admin"),
                 System.getProperty("jboss.admin.password", "jboss") };
+
+        String[] objectNames = {
+                System.getProperty("jmx.object.name0",
+                        "jboss.msc:type=container,name=jboss-as"),
+                System.getProperty("jmx.object.name1",
+                        "jboss.as:subsystem=datasources,jdbc-driver=h2"),
+                System.getProperty("jmx.object.name2",
+                        "jboss.modules:type=ModuleLoader,name=ServiceModuleLoader-5") };
 
         Map<String, Object> env = new HashMap<String, Object>();
         env.put(JMXConnectorFactory.PROTOCOL_PROVIDER_PACKAGES,
@@ -37,7 +46,12 @@ public class Main {
                     .getMBeanServerConnection();
             int count = connection.getMBeanCount();
             System.out.println("A long time ago in a galaxy far, far away.... "
-                    + count + " MBeans in the galaxy....");
+                    + count + " MBeans on the AS7....");
+            for (String objectName : objectNames) {
+                MBeanInfo beanInfo = connection.getMBeanInfo(new ObjectName(
+                        objectName));
+                System.out.println(beanInfo);
+            }
         } finally {
             jmxConnector.close();
         }
